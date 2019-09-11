c026a4ea952207c1c7e306a40b8966f503c8cb73
plumest <chayathon.k@ku.th>
2019-09-11 14:22:17 +0700

    shortcut render index page

--- polls/views.py
+++ polls/views.py
@@ -6,8 +6,8 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}.")
