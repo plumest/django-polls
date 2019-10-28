16b0930a1ccd0b82a10d6baa90c9e855323e1e61
plumest <chayathon.k@ku.th>
2019-10-28 15:06:42 +0700

    Render index pages on localhost site

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import polls.views as views
 
 urlpatterns = [
+    path('', views.IndexView.as_view()),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
