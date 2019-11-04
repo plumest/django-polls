e4d2fe4d050d0184dfc3e9b8180fde298d3e5c42
plumest <chayathon.k@ku.th>
2019-11-04 11:42:26 +0700

    Enable to login use Django Authenication

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,6 +100,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTHENTICATION_BACKENDS = (
+    # username/password authentication
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+# Redirect to '/poll/' after login
+LOGIN_REDIRECT_URL = '/polls/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
